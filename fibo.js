1efd65052a8548581f7c4415f109ba7b55383cf3
Saurabh Chauhan <imdsaurabh30@gmail.com>
2024-07-08 23:05:54 -0700

    fibo updated

--- fibo.js
+++ fibo.js
@@ -1,18 +1,12 @@
-function genfibo(n) {
-    if (n <= 0) return [0];
-   
-    if (n === 1) return [0,1]; // Return [0] if n is 1
-
-    let fibo = [0, 1]; // Initial two terms of Fibonacci sequence
-    
-   for (let i = 2; i < n; i++) {
-       let fibonext = fibo[i - 1] + fibo[i - 2];
-       fibo.push(fibonext);
-   }
-   
-   return fibo;
+function genfibo(n){
+    if (n<=0) return [0];
+    if (n==1) return [0,1];
+     var fibo=[0,1];
+    for (var i=2; i<=n; i++){
+        var fibonext = fibo[i-1]+fibo[i-2];
+            fibo.push(fibonext);
+    }
+    return fibo;
 }
-
-// Example usage:
-console.log(genfibo(3)); // Output: [0, 1, 1, 2, 3]
+    genfibo(5);
 
